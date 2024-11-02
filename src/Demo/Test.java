8f71d010aeb86fe89bcc7ca32576f8f3f1007a06
srinivas495 <srinivaschari.y@gmail.com>
2024-11-03 01:30:00 +0530

    Add Pom.xml file

--- src/Demo/Test.java
+++ src/Demo/Test.java
@@ -1,10 +1,25 @@
 package Demo;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		System.out.println("Add code in git");
+		//Invoking browser
+		//WebDriver driver = new ChromeDriver();
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/loginpagePractise/");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+
 		
+		Thread.sleep(2000);
+	//	driver.close();
+	//	driver.quit();
+		System.out.println("it's working fine");
 
 	}
 
